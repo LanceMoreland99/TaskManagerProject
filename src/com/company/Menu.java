52887adaa566d1ab5c806f709bee803c2d290030
Lance Moreland <lmoreland1899@gmail.com>
2018-02-20 20:59:24 -0500

    added remainder of the menus, added a seperate delete task prompt and display all tasks.

--- src/com/company/Menu.java
+++ src/com/company/Menu.java
@@ -11,52 +11,142 @@ public class Menu {
     public void startMenu() {
 
         System.out.println("What would you like to do?\n" +
-                            "1. Create a new task.\n" +
-                            "2. Delete an existing task.\n" +
-                            "3. View task options.\n" +
-                            "4. Exit.");
+                "1. Create a new task.\n" +
+                "2. Delete an existing task.\n" +
+                "3. View task options.\n" +
+                "4. Exit.");
 
 
-        try{
-            switch(input.nextInt()) {
+        try {
+            switch (input.nextInt()) {
 
                 case 1:
                     //Create new task.
                     Info info = new Info();
                     input.nextLine();
                     System.out.println("You have chosen to create a new task.");
+
                     //List options to create a task.
                     tasks.addTask(info);
                     break;
                 case 2:
                     //Delete a task.
-                    System.out.println("You have chosen to delete a task.\n" +
-                            "What task would you like to delete?");
-
-                    Info info1 = new Info();
-                    tasks.removeTask(input.nextInt());
+                    tasks.deleteTasks();
+                    startMenu();
                     break;
                 case 3:
-                    //View tasks.
-                    System.out.println("These are your tasks. what would you like to do?\n" +
-                            "1. View all tasks.\n" +
-                            "2. View uncompleted tasks.\n" +
-                            "3. View completed tasks.\n" +
-                            "4. Go back to the previous menu.");
-                    //Go to next menu.
-
+                    //Go to task menu.
+                    taskMenu();
                     break;
                 case 4:
                     //Exit task.
                     System.out.println("Thank you for using task manager, Good bye. ");
                     System.exit(0);
-                }
+            }
 
-        }catch (InputMismatchException ime){
+        } catch (InputMismatchException ime) {
             input.nextLine();
             System.out.println("Please input a correct number from above 1-4.\n");
-
+            startMenu();
         }
 
+    }
+
+    public void taskMenu() {
+        System.out.println("What would you like to do?\n" +
+                "1. Display all tasks\n" +
+                "2. Display uncompleted tasks\n" +
+                "3. display completed tasks.\n" +
+                "4. go back to the main menu.");
+        try {
+            switch (input.nextInt()) {
+                case 1:
+                    //All tasks options
+                    taskOptions();
+                    break;
+                case 2:
+                    //Uncompleted tasks options
+                    uncompletedTaskMenu();
+                    break;
+                case 3:
+                    //display completed tasks
+                    break;
+                case 4:
+                    //go back to main menu
+                    startMenu();
+                    break;
+            }
+        } catch (InputMismatchException ime) {
+            input.nextLine();
+            System.out.println("Please input a correct number from above 1-4.\n");
+            taskMenu();
+
+
+        }
+    }
+
+    public void taskOptions() {
+        System.out.println("What would you like to do?\n" +
+                "1. Display all tasks\n" +
+                "2. Display uncompleted tasks\n" +
+                "3. display completed tasks.\n" +
+                "4. go back to the main menu.");
+        try {
+            switch (input.nextInt()) {
+                case 1:
+                    tasks.showAllTasks(1);
+                    //display all tasks
+                    break;
+                case 2:
+                    //Edit task information
+                    break;
+                case 3:
+                    //Go back to tasks
+                    taskMenu();
+                    break;
+                case 4:
+                    //go back to main menu
+                    startMenu();
+                    break;
+            }
+        } catch (InputMismatchException ime) {
+            input.nextLine();
+            System.out.println("Please input a correct number from above 1-4.\n");
+            taskOptions();
+
+
+        }
+    }
+
+    public void uncompletedTaskMenu() {
+        System.out.println("What would you like to do?\n" +
+                "1. Display all tasks\n" +
+                "2. Display uncompleted tasks\n" +
+                "3. display completed tasks.\n" +
+                "4. go back to the main menu.");
+        try {
+            switch (input.nextInt()) {
+                case 1:
+
+                    //Mark completed tasks.
+                    break;
+                case 2:
+
+                    //Go back to task menu.
+                    taskMenu();
+                    break;
+                case 3:
+
+                    //go back to main menu.
+                    startMenu();
+                    break;
+            }
+
+        } catch (InputMismatchException ime) {
+            input.nextLine();
+            System.out.println("Please input a correct number from above 1-3.\n");
+            uncompletedTaskMenu();
+
+        }
     }
 }
--- src/com/company/Tasks.java
+++ src/com/company/Tasks.java
@@ -17,6 +17,7 @@ public class Tasks {
     public Tasks(Menu menu) {
         this.menu = menu;
     }
+
     protected void addTask(Info info) {
         //Code to add task goes here.
         System.out.println("What is the name of your task?");
@@ -33,16 +34,55 @@ public class Tasks {
         menu.startMenu();
 
     }
+
     protected void removeTask(int taskIndex) {
 
         taskIndex -= taskIndex;
         System.out.println("You have removed a task from your task lists.");
         TaskLibrary.remove(taskIndex);
 
+
         menu.startMenu();
 
     }
-    protected void showAllTasks(int taskIndex) {
 
+    protected void showAllTasks(int location) {
+
+        System.out.println("This is all your current tasks. ");
+        int index = 1;
+        for (Info info : TaskLibrary) {
+            System.out.println(index++ + ". " + info.getTaskName() + " due " + info.getDueDate());
+        }
+
+        if(TaskLibrary.isEmpty()) {
+            System.out.println("Your task list is currently empty.");
+        }
+
+        else{
+            System.out.println("You have chosen to delete a task");
+            System.out.println("What task would you like to delete?");
+        }
+        if(location == 1){
+            menu.taskOptions();
+        }
+    }
+
+    protected void deleteTasks(){
+
+        System.out.println("This is all your current tasks. ");
+        int index = 1;
+        for (Info info : TaskLibrary) {
+            System.out.println(index++ + ". " + info.getTaskName() + " due " + info.getDueDate());
+        }
+
+        if (TaskLibrary.isEmpty()) {
+            System.out.println("Your task list is currently empty.");
+        }
+
+        else{
+            System.out.println("You have chosen to delete a task");
+            System.out.println("What task would you like to delete?");
+            removeTask(input.nextInt());
+        }
     }
 }
